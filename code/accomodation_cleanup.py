c2c98b8f20f6d6bd7ee755919df8c70bec785838
carbogninalberto <alberto.carbognin@studenti.unitn.it>
2020-10-23 01:37:41 +0200

    headers

--- code/accomodation_cleanup.py
+++ code/accomodation_cleanup.py
@@ -12,20 +12,6 @@ import math
 import random
 import scipy.stats as st
 
-def loadCSV(csv_file):
-    dataset = pd.read_csv(csv_file) #, header=None) #here we are working with Pandas DataFrame
-    #print(dataset.shape)
-    data_points = []
-    column_dim = dataset.shape[1] #save the number of columns
-    row_dim = dataset.shape[0] #save the number of columns
-    if (column_dim == 1): #check if we have data organized in a single column
-        data_points = dataset[0].values.tolist() #convert the values of the column into a list of values
-    elif (column_dim > 1): #check if data is expressed as a matrix of values (multiple columns)
-        data_points = dataset.values.tolist()
-        if(row_dim == 1): #check if data is expressed as a single row
-            data_points = data_points[0]
-    return data_points
-
 def computeMean(x):
     sum_x = 0
     n = len(x)
