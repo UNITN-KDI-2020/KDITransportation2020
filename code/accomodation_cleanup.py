9e814e3013a6b8a35df2020f430af06c091e01b7
carbogninalberto <alberto.carbognin@studenti.unitn.it>
2020-10-23 01:27:57 +0200

    accomodation missing data cleanup

--- code/accomodation_cleanup.py
+++ code/accomodation_cleanup.py
@@ -2,13 +2,80 @@
 This file parse the accomodation dataset and fill
 the missing data based on the available information.
 
-We assumed that the price of the campsite fit a gaussian distribution.
+We assumed that the prices fit a gaussian distribution.
 
 @carbogninalberto
 '''
-import json
+import pandas as pd
+import numpy as np
+import math
+import random
+import scipy.stats as st
 
+def loadCSV(csv_file):
+    dataset = pd.read_csv(csv_file) #, header=None) #here we are working with Pandas DataFrame
+    #print(dataset.shape)
+    data_points = []
+    column_dim = dataset.shape[1] #save the number of columns
+    row_dim = dataset.shape[0] #save the number of columns
+    if (column_dim == 1): #check if we have data organized in a single column
+        data_points = dataset[0].values.tolist() #convert the values of the column into a list of values
+    elif (column_dim > 1): #check if data is expressed as a matrix of values (multiple columns)
+        data_points = dataset.values.tolist()
+        if(row_dim == 1): #check if data is expressed as a single row
+            data_points = data_points[0]
+    return data_points
 
+def computeMean(x):
+    sum_x = 0
+    n = len(x)
+    for i in range(0,n):
+        sum_x += float(x[i])
+    mean = sum_x / n
+    return mean
 
+def computeStdDev(x):
+    mean = computeMean(x)
+    sum_squares = 0
+    n = len(x)
+    for i in range(0,n):
+        sum_squares += pow((x[i] - mean), 2)
+    std_dev = math.sqrt(sum_squares / (n - 1))
+    return std_dev
 
-if __name__ == "___main___":
+if __name__ == "__main__":
+    print("1. Loading CSV file into a pandas dataframe")
+    dataset_path = '/mnt/c/Users/Alberto/Desktop/unitn/knowledge-and-data-integration/KDITransportation2020/dataset/Scope Definition & Inception/data/accomodation/Provincia-Autonoma-di-Trento---Elenco-strutture-extra-alberghiere.csv'
+    dataset = pd.read_csv(dataset_path, sep=";", encoding="ISO-8859-1", error_bad_lines=False, warn_bad_lines=False)
+    
+    # extracting the campsites
+    # campsites1 = dataset.loc[dataset['Tipologia']=="CAMPEGGIO"]
+    # campsites2 = dataset.loc[dataset['Tipologia']=="CAMPEGGIO PARCO PER VACANZE"]
+    # frames = [campsites1, campsites2]
+    # campsites = pd.concat(frames)
+    # print("\t- campsites dataframe:\n{}".format(campsites))
+    # print("\t- prices:\n{}".format(campsites['Prezzo']))
+
+    # extracting the affittacamere
+    extracted_typology = dataset.loc[dataset['Tipologia']=="AFFITTACAMERE"]
+    print("\t- dataframe:\n{}".format(extracted_typology))
+    
+    prices = extracted_typology['Prezzo'].tolist()
+    print("\t- prices:\n{}".format(extracted_typology['Prezzo']))
+    mean = computeMean(prices)
+    std = computeStdDev(prices)
+    print('\t- Mean: {} Std: {}'.format(mean, std))
+
+    for i in range(len(extracted_typology)):
+        if float(extracted_typology.iloc[i]['Prezzo']) == 0.0:
+            random_price = abs(np.random.normal(mean, std, 1)[0])
+            print(random_price)
+            extracted_typology.iloc[i]['Prezzo'] = random_price
+    
+    not_extracted_typology = dataset.loc[dataset['Tipologia']!="AFFITTACAMERE"]
+
+    final_dataset = pd.concat([not_extracted_typology, extracted_typology])
+    dataset_path = '/mnt/c/Users/Alberto/Desktop/unitn/knowledge-and-data-integration/KDITransportation2020/dataset/Scope Definition & Inception/data/accomodation/Provincia-Autonoma-di-Trento---Elenco-strutture-extra-alberghiere_elaborated.csv'
+    final_dataset.to_csv(dataset_path)    
+
+    
